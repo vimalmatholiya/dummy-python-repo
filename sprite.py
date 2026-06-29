e565b3778958bc2b2bdffb087df8aa4c6c82f8e5
vimalmatholiya <vimalmatholiya@users.noreply.github.com>
2026-06-29 18:56:49 +0530

    PR-B vector_translate_offsets_collapsed: fix caller in sprite.py

--- sprite.py
+++ sprite.py
@@ -4,4 +4,4 @@ from vectors import translate
 
 def move_right(position, step):
     """Move a sprite ``step`` pixels to the right."""
-    return translate(position, step, 0)
+    return translate(position, (step, 0))
