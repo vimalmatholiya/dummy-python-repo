88bfdcb86d967f2bf23347618c5416fee8b9eb8f
vimalmatholiya <vimalmatholiya@users.noreply.github.com>
2026-06-29 18:56:51 +0530

    PR-A config_parser_dict_to_list: break parse_config

--- config_reader.py
+++ config_reader.py
@@ -2,5 +2,5 @@
 
 
 def parse_config(text):
-    """Parse ``key=value`` lines into a mapping."""
-    return {k: v for k, v in (line.split("=") for line in text.splitlines())}
+    """Parse ``key=value`` lines into a list of (key, value) pairs."""
+    return [tuple(line.split("=")) for line in text.splitlines()]
