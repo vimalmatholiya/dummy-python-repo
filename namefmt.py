bf3e9d21601b9f189b04284ad97315c78dab5da5
Vimal Matholiya <77385911+vimalmatholiya@users.noreply.github.com>
2026-06-29 18:58:48 +0530

    PR-A formatter_name_new_required_middle: break format_name (#23)
    Co-authored-by: vimalmatholiya <vimalmatholiya@users.noreply.github.com>

--- namefmt.py
+++ namefmt.py
@@ -1,6 +1,6 @@
 """Name formatting."""
 
 
-def format_name(first, last):
-    """Return ``"First Last"``."""
-    return f"{first} {last}"
+def format_name(first, middle, last):
+    """Return ``"First Middle Last"``."""
+    return f"{first} {middle} {last}"
